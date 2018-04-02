f2a5b077025b17c5b22ca18717898d991813099a
Zinkou <cousin.maxime@laposte.net>
2018-04-02 14:21:40 +0200

    Initialisation appli

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name maderiseApp
+ * @description
+ * # maderiseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('maderiseApp', []);
